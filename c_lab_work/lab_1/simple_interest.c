2d9ea2b2acaa01e7fe937de5c99af4f400bc540b
Saugat Adhikari <cyberbits.predator@gmail.com>
2024-11-18 21:10:47 +0545

    lab 1 completed

--- c_lab_work/lab_1/simple_interest.c
+++ c_lab_work/lab_1/simple_interest.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 int main(){
 // Write a program to input necessary data and display the simple interest. 
-float si, p = 100000, t= 1.5, r = 10;
+float si, p,t,r;
+printf("\n Enter the p(rs) , t(yrs) , r(%) : ");
+scanf("%f%f%f",&p,&t,&r);
 si = (p*t*r)/100;
-printf("the simple interest of :( p = %.2f, t = %.2f, r = %.2f %%) is : %.2f .\n\n",p,t,r,si);
+printf("the simple interest of :( p = rs.%.2f, t = %.2f yrs, r = %.2f %%) is : %.2f .\n\n",p,t,r,si);
     return 0;
 }
